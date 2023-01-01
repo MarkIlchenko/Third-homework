667cc8e1ae3a4212e3fe31ee177471ff024e275f
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-01 16:56:48 +0200

    Сhanged the code

--- main.js
+++ main.js
@@ -9,6 +9,6 @@ console.log('Your second number is: '+ Secondnumber);
 let Thirdnumber = parseInt(prompt("Enter third number:"));
 console.log('Your third number is: '+ Thirdnumber);
 
-let Result = (Firstnumber + Secondnumber + Thirdnumber);
+let Result = (Firstnumber + Secondnumber + Thirdnumber) / 3;
 
 alert('The result of your numbers is: ' + Result);
