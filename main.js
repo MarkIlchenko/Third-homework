56edd7732cf6ee89f0b15d71f8695d41f8fbc56d
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-01 16:53:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+let Firstnumber = parseInt(prompt("Enter first number"));
+console.log('Your first number is: '+ Firstnumber);
+
+let Secondnumber = parseInt(prompt("Enter second number:"));;
+console.log('Your second number is: '+ Secondnumber);
+
+let Thirdnumber = parseInt(prompt("Enter third number:"));
+console.log('Your third number is: '+ Thirdnumber);
+
+let Result = (Firstnumber + Secondnumber + Thirdnumber);
+
+alert('The result of your numbers is: ' + Result);
