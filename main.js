51908061a5988042c57472cf9dd8ba4761d900e8
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-01 17:03:30 +0200

    Uninstalled h1, commented console.log

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 
 let Firstnumber = parseInt(prompt("Enter first number"));
-console.log('Your first number is: '+ Firstnumber);
+// console.log('Your first number is: '+ Firstnumber);
 
 let Secondnumber = parseInt(prompt("Enter second number:"));;
-console.log('Your second number is: '+ Secondnumber);
+// console.log('Your second number is: '+ Secondnumber);
 
 let Thirdnumber = parseInt(prompt("Enter third number:"));
-console.log('Your third number is: '+ Thirdnumber);
+// console.log('Your third number is: '+ Thirdnumber);
 
 let Result = (Firstnumber + Secondnumber + Thirdnumber) / 3;
 
